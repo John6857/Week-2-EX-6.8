7ac44e44c1b537a67ab088aece0e13b69105bb58
John6857 <115958166+John6857@users.noreply.github.com>
2022-10-20 17:38:17 -0400

    Initial commit

---
+++ src/For_loop.java
@@ -0,0 +1,24 @@
+
+public class For_loop
+{
+
+	public static void main(String[] args)
+	{
+		// write a for loop to print the multiples of 3 from 300 down to 3.
+		
+		int number = 3;
+		
+		for (int i = 0; i <= 100; i++) {
+		System.out.println(number + " * " + i + " = " + (number * i));
+		}
+	        
+		  
+		
+		
+		
+		
+		
+		
+		}
+
+}
